fd18d8644a161c25cdaf5f3f7d15a6af5d665b5d
turtton <top.gear7509@turtton.net>
2025-11-06 21:23:23 +0900

    Force all player enables this system

--- src/main/java/games/polarbearbytes/walktheline/ServerEvents.java
+++ src/main/java/games/polarbearbytes/walktheline/ServerEvents.java
@@ -7,7 +7,9 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Direction;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -16,7 +18,7 @@ import static net.minecraft.server.command.CommandManager.literal;
  */
 public class ServerEvents {
     public static void register() {
-        ServerLifecycleEvents.SERVER_STARTED.register((server)->WalkTheLine.server=server);
+        ServerLifecycleEvents.SERVER_STARTED.register((server) -> WalkTheLine.server = server);
         ServerWorldEvents.LOAD.register((server, world) -> WalkTheLine.server = server);
         AxisLockManager.register();
         PayloadTypeRegistry.playS2C().register(SyncPacket.PAYLOAD_ID, SyncPacket.PACKET_CODEC);
@@ -35,7 +37,35 @@ public class ServerEvents {
                         literal("walktheline")
                                 .then(literal("enable")
                                         .executes(ctx -> {
-                                            PlayerState.get().setEnabled(ctx.getSource().getPlayer(), true);
+                                            var player = ctx.getSource().getPlayer();
+                                            if (player == null) return 0;
+                                            var server = ctx.getSource().getServer();
+                                            var playerState = PlayerState.get();
+                                            playerState.setEnabled(player, true);
+                                            var data = playerState.getLockedAxisData(player);
+                                            server.getPlayerManager()
+                                                    .getPlayerList()
+                                                    .stream()
+                                                    .filter((p) -> p != player && !PlayerState.get().getEnabled(p))
+                                                    .forEach((otherPlayer) -> {
+                                                        PlayerState.get().setEnabled(otherPlayer, true);
+                                                        var pos = otherPlayer.getEntityPos();
+                                                        if (data.axis() == Direction.Axis.X) {
+                                                            otherPlayer.setPos(player.getX(), pos.getY(), pos.getZ());
+                                                        } else {
+                                                            otherPlayer.setPos(pos.getX(), pos.getY(), player.getZ());
+                                                        }
+                                                    });
+                                            server.getPlayerManager().getPlayerList().stream().filter((p) -> p != player)
+                                                    .findAny()
+                                                    .ifPresent((otherPlayer) -> {
+                                                        var pos = player.getEntityPos();
+                                                        if (data.axis() == Direction.Axis.X) {
+                                                            player.setPos(pos.getX(), pos.getY(), otherPlayer.getZ());
+                                                        } else {
+                                                            player.setPos(otherPlayer.getX(), pos.getY(), pos.getZ());
+                                                        }
+                                                    });
                                             ctx.getSource().sendFeedback(() -> Text.literal("Walk the Line enabled."), false);
                                             return 1;
                                         })
