7f983184c75d00ef203f729d37ac30b07664b7b0
sysnote8main <sysnote8main@gmail.com>
2025-12-23 21:15:54 +0900

    fix: bad implementation of end checker

--- src/main/java/games/polarbearbytes/walktheline/util/WorldUtil.java
+++ src/main/java/games/polarbearbytes/walktheline/util/WorldUtil.java
@@ -4,6 +4,6 @@ import net.minecraft.world.World;
 
 public class WorldUtil {
     public static boolean isTheEnd(World world) {
-        return "the_end".equals(world.getRegistryKey().getValue().getPath());
+        return World.END == world.getRegistryKey();
     }
 }
